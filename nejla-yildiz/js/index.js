c95001587c2b65abc003f824b6c93d567749aa0d
Tamer Aydin <tamer.aydin@gmail.com>
2018-12-07 02:43:28 +0100

    Implements toggle functionality

--- nejla-yildiz/js/index.js
+++ nejla-yildiz/js/index.js
@@ -1,9 +1,14 @@
 (function() {
   var doc = document;
   var win = window;
+  var addEventListenerMethod = win.addEventListener ? 'addEventListener' : 'attachEvent';
 
   setActivePage();
-  (win.addEventListener || win.attachEvent)('hashchange', setActivePage);
+  win[addEventListenerMethod]('hashchange', setActivePage);
+
+  addEventListenerByTagName('LABEL', 'click', function(event) {
+    setTimeout(this.parentNode.lastElementChild.scrollIntoView.bind(this), 0);
+  });
 
   function getActiveSectionFromHash() {
     return win.location.hash && win.location.hash.substr(1);
@@ -15,6 +20,13 @@
       var sectionEl = doc.getElementById(section);
       doc.body.setAttribute('data-page', section);
       sectionEl && doc.getElementById(section).scrollIntoView();
+    }
+  }
+
+  function addEventListenerByTagName(tagName, event, fn) {
+    var list = document.getElementsByTagName(tagName);
+    for (var i = 0, len = list.length; i < len; i++) {
+      list[i][addEventListenerMethod](event, fn);
     }
   }
 })();
